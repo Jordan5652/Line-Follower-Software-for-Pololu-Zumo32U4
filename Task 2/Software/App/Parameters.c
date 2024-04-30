645b7126e41b9e9380e43a7ca5219f96d5b9be25
20madmax00 <164035612+20madmax00@users.noreply.github.com>
2024-04-30 17:59:10 +0200

    added comments to variables
    in parameter.c and StateHandler.c

--- Task 2/Software/App/Parameters.c
+++ Task 2/Software/App/Parameters.c
@@ -21,8 +21,17 @@
 /* PROTOTYPES *************************************************************************************/
 
 /* VARIABLES **************************************************************************************/
+/** @var ParameterSet gSet1
+ * @brief stores the parameters for set 1
+*/
 static ParameterSet gSet1;
+/** @var ParameterSet gSet2
+ * @brief stores the parameters for set 2
+*/
 static ParameterSet gSet2;
+/** @var ParameterSet gSet3
+ * @brief stores the parameters for set 3
+*/
 static ParameterSet gSet3;
 
 /* EXTERNAL FUNCTIONS *****************************************************************************/
