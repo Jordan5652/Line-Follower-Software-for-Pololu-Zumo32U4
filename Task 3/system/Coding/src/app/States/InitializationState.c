5b41bf09f550c3c0c00217dba9a2343a8680242f
20madmax00 <164035612+20madmax00@users.noreply.github.com>
2024-05-27 14:41:07 +0200

    First Implementation of InitializationState

--- Task 3/system/Coding/src/app/States/InitializationState.c
+++ Task 3/system/Coding/src/app/States/InitializationState.c
@@ -9,9 +9,12 @@
 /**************************************************************************************************/
 
 /* INCLUDES ***************************************************************************************/
+
+
 #include "InitializationState.h"
 
 /* CONSTANTS **************************************************************************************/
+#define TEAM_NAME "Innov8tive Inc.\n" 
 
 /* MACROS *****************************************************************************************/
 
@@ -23,22 +26,26 @@
 
 /* EXTERNAL FUNCTIONS *****************************************************************************/
 
-extern void InitializationState_enterDisplayNameAndRestartTimer1(void)
+extern void InitializationState_enterDisplayNameAndStartTimer1(void)
 {
-
-
+    UInt8 buffer[] = TEAM_NAME;
+    Display_clear();
+    Display_write(buffer, sizeof(buffer));
+    SoftTimer_start(pTimer1, 2000u);
 }
 
-extern void InitializationState_leaveStopTimer1(void)
+extern void InitializationState_exitStopTimer1(void)
 {
-
-
+    SoftTimer_Stop(&)
 }
 
 extern Bool InitializationState_checkTransitionTriggerTimer1Exceeds2s(void)
 {
-
-
+    if (SOFTTIMER_IS_EXPIRED(pTimer1))
+    {
+        return true;
+    }
+    return false;
 }
 
 /* INTERNAL FUNCTIONS *****************************************************************************/
