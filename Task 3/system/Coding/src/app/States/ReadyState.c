0c52af54331b844845d90088419a9fa9baba0e36
20madmax00 <164035612+20madmax00@users.noreply.github.com>
2024-05-27 17:37:35 +0200

    First implementation of ReadyState

--- Task 3/system/Coding/src/app/States/ReadyState.c
+++ Task 3/system/Coding/src/app/States/ReadyState.c
@@ -20,30 +20,51 @@
 /* PROTOTYPES *************************************************************************************/
 
 /* VARIABLES **************************************************************************************/
+static Bool ButtonAPressed = FALSE;
+static Bool ButtonBReleased = FALSE;
+static Bool ButtonCPressed = FALSE;
 
 /* EXTERNAL FUNCTIONS *****************************************************************************/
 
 extern void ReadyState_processPollingButtons(void)
-{
+{ 
+    if(BUTTON_STATE_PRESSED == Button_getState(BUTTON_ID_A))
+    {
+        ButtonAPressed = TRUE;
+    }
 
+    if(BUTTON_STATE_RELEASED == Button_getState(BUTTON_ID_B))
+    {
+        ButtonAPressed = TRUE;
+    }
 
+    if(BUTTON_STATE_PRESSED == Button_getState(BUTTON_ID_C))
+    {
+        ButtonAPressed = TRUE;
+    }
 }
 
 extern Bool ReadyState_checkTransitionTriggerButtonAPressed(void)
 {
-
-
+    if(ButtonAPressed)
+    {
+      return TRUE;
+    }
 }
 
 extern Bool ReadyState_checkTransitionTriggerButtonBPressed(void)
 {
-
-
+    if(ButtonBReleased)
+    {
+      return TRUE;
+    }
 }
 
 extern Bool ReadyState_checkTransitionTriggerButtonCPressed(void)
 {
-
-
+    if(ButtonCPressed)
+    {
+      return TRUE;
+    }
 }
 /* INTERNAL FUNCTIONS *****************************************************************************/
