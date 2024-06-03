3948c28c9b8d84f59c3cc52958d8ec73c399db2d
20madmax00 <164035612+20madmax00@users.noreply.github.com>
2024-06-03 14:14:05 +0200

    Final changes berfore merge
    codestyle and comments

--- Task 3/system/Coding/src/app/States/LapFinishedState.h
+++ Task 3/system/Coding/src/app/States/LapFinishedState.h
@@ -28,19 +28,19 @@
 /* TYPES ******************************************************************************************/
 
 /* PROTOTYPES *************************************************************************************/
-/** 
+/**
  * @brief stops timer2 measuring the lap-time, displays the lap-time, stops the motors and plays a beep as an accustic signal
 */
 extern void LapFinishedState_enterStopTimer2AndDisplayTimeAndStopDriveAndPlayBeep(void);
 
-/** 
- * @brief checks if ButtonA is pressed by polling (ErrorState is left when ButtonA is pressed)
+/**
+ * @brief checks if ButtonA is pressed by polling
 */
 extern void LapFinishedState_processPollingButtonA(void);
 
-/** 
+/**
  * @brief checks if button A was pressed by a user
- * @return States: next State to be active or current state when no transition is true
+ * @return Bool: return true if button A was pressed
 */
 extern Bool LapFinishedState_checkTransitionTriggerButtonAPressed(void);
 
